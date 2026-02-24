ff4974cc73f22543b03d95e960637bd520f86a15
Arthur Tovmasyan <mrtag1612@gmail.com>
2026-02-24 14:37:46 +0300

    Добавил ВС

--- script.js
+++ script.js
@@ -32,16 +32,16 @@ document.querySelectorAll(".metric-row").forEach((row) => {
     row.after(chartRow);
 
     const METRIC_DATA = {
-      revenue: [120000, 300000, 450000, 380000, 420000, 500521],
-      cash: [100000, 50000, 100000, 150000, 300000, 300000],
-      noncash: [10000, 20000, 18000, 25000, 30000, 34000],
-      creditcards: [100342, 85602, 125555, 95521, 100521, 100521],
-      midcheck: [850, 500, 700, 1000, 900, 1300],
-      midguest: [750, 300, 800, 1120, 900, 1200],
-      deletafrpayment: [1500, 0, 100, 200, 1100, 1100],
-      deletbfrpayment: [1000, 700, 800, 700, 1300, 1300],
-      checks: [33, 20, 23, 30, 36, 34],
-      guests: [33, 20, 23, 30, 36, 34],
+      revenue: [120000, 120000, 300000, 450000, 380000, 420000, 500521],
+      cash: [100000, 100000, 50000, 100000, 150000, 300000, 300000],
+      noncash: [10000, 10000, 20000, 18000, 25000, 30000, 34000],
+      creditcards: [100342, 100342, 85602, 125555, 95521, 100521, 100521],
+      midcheck: [850, 850, 500, 700, 1000, 900, 1300],
+      midguest: [750, 750, 300, 800, 1120, 900, 1200],
+      deletafrpayment: [1500, 1500, 0, 100, 200, 1100, 1100],
+      deletbfrpayment: [1000, 1000, 700, 800, 700, 1300, 1300],
+      checks: [33, 33, 20, 23, 30, 36, 34],
+      guests: [33, 33, 20, 23, 30, 36, 34],
     };
 
     const key = row.dataset.metric;
@@ -54,7 +54,7 @@ document.querySelectorAll(".metric-row").forEach((row) => {
     currentChart = new Chart(canvas, {
       type: "line",
       data: {
-        labels: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+        labels: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "ВС"],
         datasets: [
           {
             label: row.cells[0].innerText,
